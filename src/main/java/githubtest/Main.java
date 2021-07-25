135fad4c039d76ef742be6a09ed45f180e231255
whdndtlr <83632789+whdndtlr@users.noreply.github.com>
2021-07-25 17:06:15 +0900

    minus java

--- src/main/java/githubtest/Main.java
+++ src/main/java/githubtest/Main.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("hello");
 	System.out.println("this java");
 	System.out.println("plus java");
+	System.out.println("minus java");
 }
 }
