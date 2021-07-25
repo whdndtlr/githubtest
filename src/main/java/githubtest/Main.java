772f5ff80c72e5a566781b694a9eefab493b704f
dndtl <dndtl@DESKTOP-SC6RQUC>
2021-07-25 17:09:11 +0900

    double java

--- src/main/java/githubtest/Main.java
+++ src/main/java/githubtest/Main.java
@@ -6,5 +6,7 @@ public static void main(String[] args) {
 	System.out.println("this java");
 	System.out.println("plus java");
 	System.out.println("minus java");
+	System.out.println("double java");
+	
 }
 }
